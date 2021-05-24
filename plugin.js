0af930453dd450fd21c33bcdb7ceff2d2c501bc9
Przemysław Kołodziejczyk <eshlox@vertolabs.com>
2021-05-24 08:56:30 +0200

    Docusaurus 2 - ESM

---
+++ plugin.js
@@ -0,0 +1,19 @@
+module.exports = function (context, options) {
+  return {
+    name: "esm-docusaurus-plugin",
+    configureWebpack(config, isServer, utils) {
+      return {
+        module: {
+          rules: [
+            {
+              // test: /\.m?js$/,
+              resolve: {
+                fullySpecified: false,
+              },
+            },
+          ],
+        },
+      };
+    },
+  };
+};
